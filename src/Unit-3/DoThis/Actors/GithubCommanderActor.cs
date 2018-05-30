9b684722127118366b5d7fdfcaea3f200638a754
Ben Camilleri <camilleriben@outlook.com>
2018-05-30 14:11:08 +0200

    Lesson 5

--- src/Unit-3/DoThis/Actors/GithubCommanderActor.cs
+++ src/Unit-3/DoThis/Actors/GithubCommanderActor.cs
@@ -51,6 +51,8 @@ namespace GithubActors.Actors
 
         private int pendingJobReplies;
 
+        private RepoKey _repoJob;
+
         public GithubCommanderActor()
         {
             Ready();
@@ -61,7 +63,7 @@ namespace GithubActors.Actors
             Receive<CanAcceptJob>(job =>
             {
                 _coordinator.Tell(job);
-
+                _repoJob = job.Repo;
                 BecomeAsking();
             });
         }
@@ -73,6 +75,9 @@ namespace GithubActors.Actors
             pendingJobReplies = _coordinator.Ask<Routees>(new GetRoutees())
               .Result.Members.Count();
             Become(Asking);
+
+            // send ourselves a ReceiveTimeout message if no message within 3 seconds
+            Context.SetReceiveTimeout(TimeSpan.FromSeconds(3));
         }
 
 
@@ -104,12 +109,22 @@ namespace GithubActors.Actors
 
                 BecomeReady();
             });
+
+            // means at least one actor failed to respond
+            Receive<ReceiveTimeout>(timeout =>
+            {
+                _canAcceptJobSender.Tell(new UnableToAcceptJob(_repoJob));
+                BecomeReady();
+            });
         }
 
         private void BecomeReady()
         {
             Become(Ready);
             Stash.UnstashAll();
+
+            // cancel ReceiveTimeout
+            Context.SetReceiveTimeout(null);
         }
 
         protected override void PreStart()
